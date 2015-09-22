5bf27818b7a209c5b962830874bfe7b221b6d90c
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2015-09-22 21:37:39 +0200

    Added View
    XAML

--- dev/xamarin/LoneWolf/LoneWolf/LoneWolf/App.cs
+++ dev/xamarin/LoneWolf/LoneWolf/LoneWolf/App.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using LoneWolf.Views;
 using Xamarin.Forms;
 
 namespace LoneWolf
@@ -12,19 +12,7 @@ namespace LoneWolf
 		public App()
 		{
 			// The root page of your application
-			MainPage = new ContentPage
-			{
-				Content = new StackLayout
-				{
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							XAlign = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
+			MainPage = new MainPage();
 		}
 
 		protected override void OnStart()
