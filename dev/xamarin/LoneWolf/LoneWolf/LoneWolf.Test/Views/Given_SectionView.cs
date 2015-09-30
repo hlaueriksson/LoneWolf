7a20948f9c816daa1dca0b73520a99f7f00c0442
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2015-09-30 18:17:58 +0200

    Added test project
    from VS2013

---
+++ dev/xamarin/LoneWolf/LoneWolf/LoneWolf.Test/Views/Given_SectionView.cs
@@ -0,0 +1,22 @@
+﻿using LoneWolf.Models;
+using Machine.Specifications;
+
+namespace LoneWolf.Test.Views
+{
+	[Subject(typeof(SectionView))]
+	public class GenerateString
+	{
+		static SectionView subject;
+
+		Establish context = () =>
+		{
+			subject = new SectionView();
+		};
+
+		It should_render_valid_model = () =>
+		{
+			subject.Model = new Section { Number = "1", Choices = new[] { new Choice { Number = "2" } } };
+			subject.GenerateString().ShouldNotBeEmpty();
+		};
+	}
+}
