b9c6a68bcad92581888334ae6167a4d88bb1cc3b
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2016-09-02 11:06:55 +0200

    Moved code to the LoneWolf.iOS project

---
+++ dev/xamarin/LoneWolf/LoneWolf/LoneWolf.iOS/BaseUrlProxy.cs
@@ -0,0 +1,15 @@
+﻿using Foundation;
+using LoneWolf.iOS;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(BaseUrlProxy))]
+namespace LoneWolf.iOS
+{
+	public class BaseUrlProxy : IBaseUrl
+	{
+		public string Get()
+		{
+			return NSBundle.MainBundle.BundlePath;
+		}
+	}
+}
---
+++ dev/xamarin/LoneWolf/LoneWolf/LoneWolf.iOS/SectionReaderProxy.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using LoneWolf.iOS;
+using LoneWolf.Models;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SectionReaderProxy))]
+namespace LoneWolf.iOS
+{
+	public class SectionReaderProxy : ISectionReader
+	{
+		public string Read(SectionReference number)
+		{
+			var path = $"sections/sect{number.Value.PadLeft(3, '0')}.json";
+
+			return File.ReadAllText(path);
+		}
+	}
+}
